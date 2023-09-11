0faa4f04382cfb6773b3949adf81741b4b550efd
Jeffry Gonzalez <Jeff@hypertheory.com>
2023-09-11 14:33:39 -0400

    done with get

--- DemoApi/Controllers/InfoController.cs
+++ DemoApi/Controllers/InfoController.cs
@@ -9,4 +9,49 @@ public class InfoController : ControllerBase
     {
         return Ok($"The Controller is working just fine. created at { DateTime.Now.ToLongTimeString()}");
     }
+
+    // GET /blog/2023/9/11
+    [HttpGet("/blog/{year:int:min(2005)}/{month:int:min(1):max(12)}/{day:int}")]
+    public async Task<ActionResult> GetTheBlogStuff(int year, int month, int day)
+    {
+        if(month == 2 && day > 29)
+        {
+            return NotFound();
+        }
+        return Ok($"Showing the blog stuff for {year} {month} {day}");
+    }
+
+    // GET /colors?color=Green
+    [HttpGet("/colors")]
+    public async Task<ActionResult> GetColors([FromQuery] string color = "Blue")
+    {
+        return Ok($"You Picked {color}");
+    }
+    // GET /employees
+    [HttpGet("/employees")]
+    public async Task<ActionResult> GetEmployees([FromQuery] string department = "All")
+    {
+        var employees = new List<Employee>
+        {
+            new Employee("Bob Smith", "dev"),
+            new Employee("Joe Jones", "dev"),
+            new Employee("Sue Blue", "ceo")
+        };
+        if(department != "All")
+        {
+            var response = employees.Where(e => e.Department == department).ToList();
+            return Ok(new ResponseType<List<Employee>>(response, department));
+        }
+        return Ok(new ResponseType<List<Employee>>(employees, department));
+    }
+
+    [HttpGet("/whoami")]
+    public async Task<ActionResult> WhoAmI([FromHeader(Name ="User-Agent")]string userAgent, [FromHeader(Name ="X-Tacos")] string tacos)
+    {
+        return Ok($"You are running {userAgent} and you think tacos are {tacos}");
+    }
 }
+
+
+public record ResponseType<T>(T data, string Filter);
+public record Employee(string Name, string Department);
